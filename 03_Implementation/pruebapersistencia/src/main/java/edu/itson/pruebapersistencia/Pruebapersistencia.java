5ce476ebc21e28476ef4bc5d92a289d0ad60bd02
SilverFlin <toledorusso@outlook.com>
2023-06-12 21:23:28 -0700

    chore: proyecto pruebas

---
+++ 03_Implementation/pruebapersistencia/src/main/java/edu/itson/pruebapersistencia/Pruebapersistencia.java
@@ -0,0 +1,29 @@
+package edu.itson.pruebapersistencia;
+
+/**
+ *
+ */
+public class Pruebapersistencia {
+
+    public static void main(String[] args) {
+        probarDaos();
+    }
+
+    private static void probarComentariosDao() {
+        // TODO
+    }
+
+    private static void probarPostsDao() {
+        // TODO
+    }
+
+    private static void probarUsuariosDao() {
+        // TODO
+    }
+
+    private static void probarDaos() {
+        probarComentariosDao();
+        probarPostsDao();
+        probarUsuariosDao();
+    }
+}
