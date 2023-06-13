cd28a97b68bd869962a35d5cb38a810e6513d6bc
SilverFlin <toledorusso@outlook.com>
2023-06-13 16:25:12 -0700

    fix: persistencia exception

--- 03_Implementation/persistencia/src/main/java/exceptions/PersistenciaException.java
+++ 03_Implementation/persistencia/src/main/java/exceptions/PersistenciaException.java
@@ -1,47 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package excepciones;
+package exceptions;
 
 /**
  *
- * @author MoonA
  */
 public final class PersistenciaException extends Exception {
 
     /**
-     * Constructor por omisión de la clase
+     * Constructor por omisión de la clase.
      */
     public PersistenciaException() {
     }
 
     /**
-     * Constructor de la clase que recibe un parámetro
+     * Constructor de la clase que recibe un parámetro.
      *
      * @param message mensaje recibido a almancenar
      */
-    public PersistenciaException(String message) {
+    public PersistenciaException(final String message) {
         super(message);
     }
 
     /**
      * Constructor que recibe un mensaje y la causa del lanzamiento de la
-     * excepción
+     * excepción.
      *
      * @param message mensaje recibido a almacenar
      * @param cause causa que ocasionó el lanzamiento de la excepción
      */
-    public PersistenciaException(String message, Throwable cause) {
+    public PersistenciaException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
     /**
-     * Constructor que solo recibe la causa del lanzamiento de la excepción
+     * Constructor que solo recibe la causa del lanzamiento de la excepción.
      *
      * @param cause causa que lo ocasionó
      */
-    public PersistenciaException(Throwable cause) {
+    public PersistenciaException(final Throwable cause) {
         super(cause);
     }
 }
