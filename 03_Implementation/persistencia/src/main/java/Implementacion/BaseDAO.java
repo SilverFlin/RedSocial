07f7db6c6cbda0510312ec33f75e85fa0c200094
GerardoZillmann <gerardozillmann1@gmail.com>
2023-06-12 17:51:03 -0700

    chore: baseDAO DAOException
    clases de base DAO y DAOException

---
+++ 03_Implementation/persistencia/src/main/java/Implementacion/BaseDAO.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Implementacion;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import exceptions.DAOException;
+import java.util.ArrayList;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.types.ObjectId;
+
+public abstract class BaseDAO<T> {
+
+    public MongoClient getConexionMongoClient() {
+        //Creamos un proveedor de codecs para las clases POJOs
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+        //Creamos la configuración del cliente de MongoDB      
+        MongoClient mongoC = new MongoClient("localhost", MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build());
+        return mongoC;
+    }
+
+    public MongoDatabase getMongoDB(String nomBD) {
+        return getConexionMongoClient().getDatabase(nomBD);
+    }
+
+    public abstract void guardar(T entidad);
+
+    public abstract void eliminar(T entidad) throws DAOException;
+
+    public abstract void actualizar(T entidad) throws DAOException;
+
+    public abstract T buscarPorID(ObjectId id);
+
+    public abstract ArrayList<T> buscarTodos();
+
+    public abstract MongoCollection<T> getCollection();
+}
---
+++ 03_Implementation/persistencia/src/main/java/exceptions/DAOException.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+
+public class DAOException extends Exception {
+    public DAOException(){
+        
+    }
+    public DAOException(String n){
+        super(n);
+    }
+    
+    public DAOException(String n, Throwable t){
+        super(n,t);
+    }
+    
+    public DAOException(Throwable t){
+        super(t);
+    }
+    
+}
