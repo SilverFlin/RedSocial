1e5c4d1776a3d07ef26711b678a5f9523b699c37
SilverFlin <toledorusso@outlook.com>
2023-07-02 13:00:14 -0700

    feat!: create post servlet

---
+++ 03_Implementation/webapp/src/main/java/edu/itson/webapp/servlets/CreatePostServlet.java
@@ -0,0 +1,193 @@
+package edu.itson.webapp.servlets;
+
+import edu.itson.dominio.ContenidoPost;
+import edu.itson.dominio.Post;
+import edu.itson.dominio.TipoPost;
+import edu.itson.dominio.Usuario;
+import edu.itson.webapp.business.impl.PostsBO;
+import edu.itson.webapp.business.interfaces.IPostBO;
+import edu.itson.webapp.exceptions.BusinessException;
+import edu.itson.webapp.http.HttpStatusCode;
+import edu.itson.webapp.paths.Constants;
+import edu.itson.webapp.utils.impl.FormValidator;
+import edu.itson.webapp.utils.interfaces.IFormValidator;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ */
+@WebServlet(name = "CreatePostServlet", urlPatterns = {"/create-post"})
+public class CreatePostServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param req servlet request
+     * @param res servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(
+            final HttpServletRequest req,
+            final HttpServletResponse res
+    )
+            throws ServletException, IOException {
+        String action = req.getParameter("action");
+
+        /* Default Action */
+        if (action == null || action.equalsIgnoreCase("create-post")) {
+            this.sendToCreatePostPage(req, res);
+            return;
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param req servlet request
+     * @param res servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(
+            final HttpServletRequest req,
+            final HttpServletResponse res
+    )
+            throws ServletException, IOException {
+        String action = req.getParameter("action");
+
+        if (action == null || action.equalsIgnoreCase("create-post")) {
+            this.processCreatePost(req, res);
+            return;
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+    private void sendToCreatePostPage(
+            final HttpServletRequest req,
+            final HttpServletResponse res
+    )
+            throws ServletException, IOException {
+        getServletContext()
+                .getRequestDispatcher(Constants.CREATE_POST_PAGE)
+                .forward(req, res);
+    }
+
+    private void processCreatePost(
+            final HttpServletRequest req,
+            final HttpServletResponse res
+    )
+            throws ServletException, IOException {
+        String titleParam = req.getParameter("title");
+        String contentParam = req.getParameter("content");
+        // TODO Image File
+
+        if (!this.validateParams(titleParam, contentParam)) {
+            this.sendToHttpErrorPage(req, res, HttpStatusCode.BAD_REQUEST);
+            return;
+        }
+
+        Usuario loggedUser = (Usuario) req.getSession().getAttribute("user");
+
+        if (loggedUser == null) {
+            this.sendToHttpErrorPage(req, res, HttpStatusCode.UNAUTHORIZED);
+            return;
+        }
+
+        Post postCreated;
+        try {
+            postCreated
+                    = this.tryCreatePost(titleParam, contentParam, loggedUser);
+        } catch (BusinessException ex) {
+            this.sendToServerErrorPage(req, res);
+            return;
+        }
+
+        if (postCreated == null) {
+            this.sendToHttpErrorPage(req, res, HttpStatusCode.BAD_REQUEST);
+            return;
+        }
+
+        this.redirectHome(req, res);
+    }
+
+    private boolean validateParams(final String title, final String content) {
+        IFormValidator validator = new FormValidator();
+
+        final int titleLimit = 15;
+        boolean isValidTitle
+                = !validator.hasBlankSpaces(title)
+                && !validator.hasExceededLengthLimit(title, titleLimit);
+
+        final int contentLimit = 500;
+        boolean isValidContent
+                = !validator.hasBlankSpaces(content)
+                && !validator.hasExceededLengthLimit(content, contentLimit);
+
+        return isValidTitle && isValidContent;
+    }
+
+    private Post tryCreatePost(
+            final String titleParam,
+            final String contentParam,
+            final Usuario user
+    ) throws BusinessException {
+        Post postCreated = new Post(TipoPost.NORMAL);
+        postCreated.setTitulo(titleParam);
+        ContenidoPost contenidoPost = new ContenidoPost();
+        contenidoPost.setTexto(contentParam);
+        postCreated.setContenido(contenidoPost);
+        postCreated.setCreador(user);
+        postCreated.setFechaHoraCreacion(LocalDateTime.now());
+        IPostBO postBO = new PostsBO();
+        return postBO.createPost(postCreated);
+    }
+
+    private void sendToHttpErrorPage(
+            final HttpServletRequest req,
+            final HttpServletResponse res,
+            final HttpStatusCode httpStatusCode
+    ) throws ServletException, IOException {
+        res.setStatus(httpStatusCode.getCode());
+        getServletContext()
+                .getRequestDispatcher(Constants.HTTP_ERROR_PAGE)
+                .forward(req, res);
+    }
+
+    private void sendToServerErrorPage(
+            final HttpServletRequest req,
+            final HttpServletResponse res
+    ) throws ServletException, IOException {
+        res.setStatus(HttpStatusCode.INTERNAL_SERVER_ERROR.getCode());
+        getServletContext()
+                .getRequestDispatcher(Constants.SERVER_ERROR_PAGE)
+                .forward(req, res);
+    }
+
+    private void redirectHome(
+            final HttpServletRequest req,
+            final HttpServletResponse res
+    ) throws IOException {
+        res.setStatus(HttpStatusCode.OK.getCode());
+        res.sendRedirect(req.getContextPath() + Constants.HOME_ENDPOINT);
+    }
+
+}
