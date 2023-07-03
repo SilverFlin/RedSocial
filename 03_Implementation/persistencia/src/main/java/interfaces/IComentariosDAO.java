1307dca08180a9a7977a2b7629096bd0fc39e080
ArturoLuna7 <arturo712luna@gmail.com>
2023-07-02 19:16:04 -0700

    chore: add-search-comments-post
    Se agregó un método para buscar comentarios por post.

--- 03_Implementation/persistencia/src/main/java/interfaces/IComentariosDAO.java
+++ 03_Implementation/persistencia/src/main/java/interfaces/IComentariosDAO.java
@@ -1,10 +1,20 @@
 package interfaces;
 
 import edu.itson.dominio.Comentario;
+import exceptions.PersistenciaException;
+import java.util.List;
 
 /**
  *
  */
 public interface IComentariosDAO extends IBaseDAO<Comentario> {
 
+    /**
+     * Método que permite obtener comentarios de un post.
+     *
+     * @param objetivo
+     * @return una lista de comentarios
+     */
+    List<Comentario> buscarComentariosPost(Comentable objetivo)
+            throws PersistenciaException;
 }
